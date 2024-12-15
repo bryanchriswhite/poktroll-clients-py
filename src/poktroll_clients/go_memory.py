5520dba0a51b2ddf92b57e4d219af7594f8d3e82
Bryan White <bryanchriswhite@gmail.com>
2024-12-15 22:03:03 +0100

    refactor: raname self_ref var for consistency

--- src/poktroll_clients/go_memory.py
+++ src/poktroll_clients/go_memory.py
@@ -34,15 +34,15 @@ class GoManagedMem:
     go_ref: go_ref
     err_ptr: ffi.CData = ffi.new("char **")
 
-    def __init__(self, go_ref: go_ref):
+    def __init__(self, self_ref: go_ref):
         """
         Constructor for GoManagedMem. Stores the Go-managed memory reference.
         """
 
-        self.go_ref = go_ref
+        self.go_ref = self_ref
 
         check_err(self.err_ptr)
-        check_ref(go_ref)
+        check_ref(self_ref)
 
     def __del__(self):
         """
